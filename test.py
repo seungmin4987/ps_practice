a805d881ef2e8a98637f0c2dce362bb97ff467bd
seungmin <seungmin4987@naver.com>
2025-09-03 14:02:01 +0900

    5597

---
+++ test.py
@@ -0,0 +1,13 @@
+check = [0]*31
+
+for i in range(28):
+    n = int(input())
+    check[n] = 1
+
+answer = []
+for i in range(1,31):
+    if check[i] == 0:
+        answer.append(i)
+
+print(answer[0])
+print(answer[1])
